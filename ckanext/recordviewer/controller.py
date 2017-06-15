b9c7d93ee16bc7e988227047e37cec98658c6493
root <fernando.bac03@hotmail.com>
2017-06-15 19:05:58 +0200

    add link for view record information

--- ckanext/recordviewer/controller.py
+++ ckanext/recordviewer/controller.py
@@ -535,7 +535,7 @@ class RVController(BaseController):
                 'error_summary': error_summary, 'action': 'edit',
      #           'resource_form_snippet': self._resource_form(package_type),
                 'dataset_type': package_type}
-        return render('package/resource_edit.html', extra_vars=vars)
+        return render('record/view_record.html', extra_vars=vars)
 
 
 
